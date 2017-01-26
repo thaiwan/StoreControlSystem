553aa9e954b100fa7d7f2ec1c5aac3328d44572c
Tatiana Mitioglova <thaiwan7@gmail.com>
2017-01-26 03:32:43 +0300

    Database was updated

--- webstoredb.sql
+++ webstoredb.sql
@@ -26,7 +26,7 @@ CREATE TABLE `product` (
   `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +35,35 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (3,'Кабачок'),(7,'Огурец'),(8,'Баклажан'),(10,'Морковь'),(11,'Помидор');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_transaction`
+--
+
+DROP TABLE IF EXISTS `product_transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_transaction` (
+  `product_product_id` int(11) NOT NULL,
+  `transactions_transaction_id` int(11) NOT NULL,
+  UNIQUE KEY `UK_hvk0nta69igftlbhoymnm539q` (`transactions_transaction_id`),
+  KEY `FK_hvk0nta69igftlbhoymnm539q` (`transactions_transaction_id`),
+  KEY `FK_p4gqfvofdgx4n3mo8xakukfo6` (`product_product_id`),
+  CONSTRAINT `FK_hvk0nta69igftlbhoymnm539q` FOREIGN KEY (`transactions_transaction_id`) REFERENCES `transaction` (`transaction_id`),
+  CONSTRAINT `FK_p4gqfvofdgx4n3mo8xakukfo6` FOREIGN KEY (`product_product_id`) REFERENCES `product` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_transaction`
+--
+
+LOCK TABLES `product_transaction` WRITE;
+/*!40000 ALTER TABLE `product_transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -49,7 +77,7 @@ CREATE TABLE `store` (
   `store_id` int(11) NOT NULL AUTO_INCREMENT,
   `address` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`store_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -58,7 +86,35 @@ CREATE TABLE `store` (
 
 LOCK TABLES `store` WRITE;
 /*!40000 ALTER TABLE `store` DISABLE KEYS */;
+INSERT INTO `store` VALUES (6,'Петровка 38'),(8,'Шувалова 7'),(11,'Дворцовая 7');
 /*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store_transaction`
+--
+
+DROP TABLE IF EXISTS `store_transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store_transaction` (
+  `store_store_id` int(11) NOT NULL,
+  `transactions_transaction_id` int(11) NOT NULL,
+  UNIQUE KEY `UK_1flmi11dcorchuvy8qiap7182` (`transactions_transaction_id`),
+  KEY `FK_1flmi11dcorchuvy8qiap7182` (`transactions_transaction_id`),
+  KEY `FK_906igdd7s9qr0ftegyktr0uum` (`store_store_id`),
+  CONSTRAINT `FK_1flmi11dcorchuvy8qiap7182` FOREIGN KEY (`transactions_transaction_id`) REFERENCES `transaction` (`transaction_id`),
+  CONSTRAINT `FK_906igdd7s9qr0ftegyktr0uum` FOREIGN KEY (`store_store_id`) REFERENCES `store` (`store_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store_transaction`
+--
+
+LOCK TABLES `store_transaction` WRITE;
+/*!40000 ALTER TABLE `store_transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `store_transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -72,10 +128,15 @@ CREATE TABLE `transaction` (
   `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
   `transaction_date` datetime NOT NULL,
   `store` int(11) NOT NULL,
+  `count` int(11) DEFAULT NULL,
+  `cost` int(11) DEFAULT NULL,
+  `product_id` int(11) NOT NULL,
   PRIMARY KEY (`transaction_id`),
   KEY `fk_transaction_store_idx` (`store`),
+  KEY `fk_transaction_product1_idx` (`product_id`),
+  CONSTRAINT `fk_transaction_product1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_transaction_store` FOREIGN KEY (`store`) REFERENCES `store` (`store_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,35 +145,8 @@ CREATE TABLE `transaction` (
 
 LOCK TABLES `transaction` WRITE;
 /*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES (1,'2017-01-24 00:00:00',6,1,1,3),(2,'2017-01-24 00:00:00',6,545,444,3),(3,'2017-01-24 00:00:00',8,1,1,3),(5,'2017-01-25 00:00:00',6,5,5,3),(8,'2017-01-25 00:00:00',6,111,111,7),(9,'2017-01-26 00:00:00',6,1,1,7),(11,'2017-01-11 00:00:00',6,3,454,3),(12,'2017-01-26 00:00:00',6,1,1,8);
 /*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `transaction_product`
---
-
-DROP TABLE IF EXISTS `transaction_product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `transaction_product` (
-  `transaction_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `count` int(11) DEFAULT NULL,
-  `cost` int(11) DEFAULT NULL,
-  KEY `fk_transaction_product_transaction1_idx` (`transaction_id`),
-  KEY `fk_transaction_product_product1_idx` (`product_id`),
-  CONSTRAINT `fk_transaction_product_product1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transaction_product_transaction1` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `transaction_product`
---
-
-LOCK TABLES `transaction_product` WRITE;
-/*!40000 ALTER TABLE `transaction_product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transaction_product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -124,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-01-20  1:31:29
+-- Dump completed on 2017-01-26  3:32:06
